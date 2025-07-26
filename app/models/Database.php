94f346cf750d7d3dd2d7807883a9b986c293f7ea
amirlanqz <amirlanqz@gmail.com>
2025-07-27 01:58:39 +0500

    check exist user table, if not users then we need to create

--- app/models/Database.php
+++ app/models/Database.php
@@ -4,17 +4,19 @@ class Database
 {
     private static $instance = null;
     private $conn;
-    private $host = "mysql";
-    private $user = "user";
-    private $pass = 'secret';
-    private $name = 'app';
-
 
     private function __construct(){
-        $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8mb4";
+
+        $config = require_once __DIR__ . "/../../config.php";
+        $db_host = $config['db_host'];
+        $db_user = $config['db_user'];
+        $db_password = $config['db_password'];
+        $db_name = $config['db_name'];
+
+        $dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4";
 
         try {
-            $this->conn = new \PDO($dsn, $this->user, $this->pass, [
+            $this->conn = new \PDO($dsn, $db_user, $db_password, [
                 \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
                 \PDO::ATTR_EMULATE_PREPARES => false,
@@ -25,8 +27,8 @@ class Database
     }
 
     public static function getInstance() {
-        if (!self::$instance) {
-            self::$instance = new Database();
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
         }
         return self::$instance;
     }
--- app/models/User.php
+++ app/models/User.php
@@ -9,6 +9,24 @@ class User
     public function __construct()
     {
         $this->db = Database::getInstance()->getConnection();
+        try {
+            $result = $this->db->query("SELECT 1 FROM users limit 1");
+        } catch (PDOException $e) {
+            $this->createTable();
+        }
+    }
+
+    public function createTable(): bool
+    {
+        $query = "CREATE TABLE IF NOT EXISTS `users` (
+            `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            `login` VARCHAR(255) NOT NULL,
+            `password` VARCHAR(255) NOT NULL,
+            `is_admin` TINYINT(1) NOT NULL DEFAULT '0',
+            `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )";
+        $this->db->exec($query);
+        return true;
     }
 
     public function readAll() {
@@ -54,7 +72,7 @@ class User
     {
         $stmt =  $this->db->prepare("SELECT * FROM users where id = ?");
         $stmt->execute([$id]);
-        $user = $stmt->fetch();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
         return $user;
     }
 
